ff485e73db70ca2d0f5e6a7a914cabd9d52d7d70
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-04-09 09:29:07 -0500

    Minor changes to functions and pages
    1. Move GMSO at the top and apply spacing in bottom two repos
    2. Consolidate functions to populate releases

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
             // console.log('clicked', $('#ecoSystem').offset().top);
             $("body, html").animate({
                 'scrollTop':   $('#ecoSystem').offset().top
-            }, 500); 
+            }, 500);
         };
         }
     })();
@@ -19,14 +19,14 @@ $(document).ready(function() {
                     $('#brandLogo').toggleClass('hidden');
                 },
                 onCloseStart: () => {
-                    $('#main').toggleClass('push-left');   
+                    $('#main').toggleClass('push-left');
                     $('#brandLogo').toggleClass('hidden');
-                }    
+                }
     }); // Initialize the sidenav
 
     $('#close-sidenav').click(function() {
         $('.sidenav').sidenav('close'); // Close the sidenav
-        
+
     });
 
 
@@ -35,66 +35,4 @@ $(document).ready(function() {
         indicators: true
     });
 
-    let htmlContent = '';
-    fetch('https://api.github.com/orgs/mosdef-hub/repos').then(
-        (response) => {
-            return response.json()
-        })
-        .then((reposList) => {
-            let count = 0;
-            reposList.forEach((repo, index, records) => {
-                let description = repo.description;
-                let name = repo.name
-                name = name.split('_').join(" ");
-                if (description != null && name != 'msibi'){
-                if (name == 'mbuild' || name === 'foyer' or name === 'gmso'){
-                    let docsUrl = "";
-                    if (name==='mbuild'){
-                        docsUrl = "http://mosdef-hub.github.io/mbuild/";
-                    }
-                    else if (name==='foyer'){
-                        docsUrl = "http://mosdef-hub.github.io/foyer/";
-		    }
-		    else {
-			docsUrl = "http://mosdef-hub.github.io/gmso/";
-		    }
-                    }
-                    htmlContent += `
-                        <div class="col s12 l4">
-                        <div class="card small banner-background">
-                        <div class="card-content white-text">
-                        <div class="card-title">${name}</div>
-                        <p>${description}</p>
-                        <a  style="margin-right:30%" class="btn-floating halfway-fab waves-effect waves-light black"><i class="material-icons red-text">star</i></a>
-                        <a  style="margin-right:15%" class="btn-floating halfway-fab waves-effect waves-light black" href=${docsUrl} target="_blank"><i class="material-icons">more</i></a>
-                        <a class="btn-floating halfway-fab waves-effect waves-light black" href=${repo.html_url} target="_blank"><i class="fa fa-github"></i></a>
-                        
-                        </div>
-                        </div>
-                     </div>`;
-                        count++;
-                } else{
-                    htmlContent += `
-                        <div class="col s12 l4">
-                        <div class="card small banner-background">
-                        <div class="card-content white-text">
-                        <div class="card-title">${name}</div>
-                        <p>${description}</p>
-                        <a class="btn-floating halfway-fab waves-effect waves-light black" href=${repo.html_url} target="_blank"><i class="fa fa-github"></i></a>
-                        </div>
-                        </div>
-                     </div>`;
-                        count++;
-                    }
-                }
-                
-                
-            });
-            // No longer necessary, decided to hard code
-            // console.log(htmlContent);
-            // let reposDiv = document.getElementById('ecoSystem');
-            // if (reposDiv !== null){
-            //     reposDiv.innerHTML = htmlContent;
-            // }
-        });
 });
