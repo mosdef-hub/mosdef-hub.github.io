90cfc50346dc0a2b363b6625825f81963627aef1
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-06-16 20:55:24 -0500

    Add Grant Numbers and PI Info (Closes #13) (#14)

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
-$(document).ready(function() {
-
-     // get started button scrolling
-     (function() {
+$(document).ready(function () {
+    let isPaused = false;
+    // get started button scrolling
+    (function () {
         var btn = document.getElementById('get-started-btn');
         var tgtSection = document.getElementById('ecoSystem');
-        if(btn != null){
-        btn.onclick = function() {
-            // console.log('clicked', $('#ecoSystem').offset().top);
-            $("body, html").animate({
-                'scrollTop':   $('#ecoSystem').offset().top
-            }, 500);
-        };
+        if (btn != null) {
+            btn.onclick = function () {
+                // console.log('clicked', $('#ecoSystem').offset().top);
+                $("body, html").animate({
+                    'scrollTop': $('#ecoSystem').offset().top
+                }, 500);
+            };
         }
     })();
 
-    $('.sidenav').sidenav({onOpenStart: () => {
-                    $('#main').toggleClass('push-left');
-                    $('#brandLogo').toggleClass('hidden');
-                },
-                onCloseStart: () => {
-                    $('#main').toggleClass('push-left');
-                    $('#brandLogo').toggleClass('hidden');
-                }
+    $('.sidenav').sidenav({
+        onOpenStart: () => {
+            $('#main').toggleClass('push-left');
+            $('#brandLogo').toggleClass('hidden');
+        },
+        onCloseStart: () => {
+            $('#main').toggleClass('push-left');
+            $('#brandLogo').toggleClass('hidden');
+        }
     }); // Initialize the sidenav
 
-    $('#close-sidenav').click(function() {
+    $('#close-sidenav').click(function () {
         $('.sidenav').sidenav('close'); // Close the sidenav
 
     });
@@ -34,5 +35,21 @@ $(document).ready(function() {
         fullWidth: true,
         indicators: true
     });
+
+    $('.carousel').carousel({
+        duration: 200
+    });
+
+    setInterval(() => {
+        if (!isPaused) {
+            $('.carousel').carousel('next')
+        }
+    }, 1000);
+
+    $('.carousel').hover(() => {
+        isPaused = true;
+    }, () => {
+        isPaused = false;
+    })
 
 });
